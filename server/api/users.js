e0d6c3c9c21a5eb1365b838f4e15aa0cab2f0fa6
Treagan Birbal <treaganbirbal@pursuit.org>
2020-07-05 18:49:13 -0400

    create new branch for PR

---
+++ server/api/users.js
@@ -0,0 +1,92 @@
+const router = require("express").Router();
+const { User, Budget, Transaction, Account } = require("../db/model");
+module.exports = router;
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const data = await User.findOne({
+      where: { id: userId },
+      include: [{ model: Account }, { model: Budget }, { model: Transaction }],
+    });
+    if (data) {
+      res.json(data);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body);
+    if (newUser) {
+      res.json({ message: "Created Successfully", newUser });
+    } else {
+      res.sendStatus(500);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    await User.update(
+      {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+        userId: req.body.userId,
+        userName: req.body.userName,
+      },
+      {
+        where: {
+          id: userId,
+        },
+      }
+    );
+    if (!userId) {
+      res.sendStatus(500);
+    } else {
+      const updatedUser = await Project.findOne({
+        where: { id: userId },
+      });
+      res.json({
+        message: "User updated successfully",
+        project: updatedUser,
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const user = User.findOne({
+      where: {
+        id: userId,
+      },
+    });
+    if (user) {
+      User.destroy({
+        where: {
+          id: userId,
+        },
+      });
+      res.json({
+        message: "User Deleted successfully!",
+      });
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
