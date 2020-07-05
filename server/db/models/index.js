a0d4d6bf376b6b8c46be3e18d8c224f9bd66b122
Mariia Romaniuk <m.grabelnikova@gmail.com>
2020-07-05 17:46:53 -0400

    Associations between models defined

---
+++ server/db/models/index.js
@@ -0,0 +1,30 @@
+const User = require('./user');
+const Account = require('./account');
+const Transaction = require('./transaction');
+const Budget = require('./budget');
+
+/*
+ * Associations between the models.
+ */
+
+User.hasMany(Account);
+Account.belongsTo(User);
+
+User.hasMany(Transaction);
+Transaction.belongsTo(User);
+
+User.hasOne(Budget);
+Budget.belongsTo(User);
+
+/*
+ * We'll export all of our models here, so that any time a module needs
+ * a model, we can just require it from 'db/models'
+ * for example, we can say: const {User} = require('../db/models')
+ * instead of: const User = require('../db/models/user')
+ */
+module.exports = {
+  User,
+  Account,
+  Transaction,
+  Budget,
+};
