b9aad67393abdddcaffb1a79cd627dd6283546d6
Mariia Romaniuk <m.grabelnikova@gmail.com>
2020-07-06 15:13:06 -0400

    Login signup router added

---
+++ server/auth/index.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+const User = require('../db/models/user');
+
+module.exports = router;
+
+router.post('/login', (req, res, next) => {
+  User.findOne({ where: { email: req.body.email } })
+    .then(user => {
+      if (!user) {
+        console.log('No such user found:', req.body.email);
+        res.status(401).send('Wrong username and/or password');
+      } else if (!user.correctPassword(req.body.password)) {
+        console.log('Incorrect password for user:', req.body.email);
+        res.status(401).send('Wrong username and/or password');
+      } else {
+        let currentDate = new Date();
+        let userId = user.id;
+        user.update({ pushToken: req.body.pushToken, lastLogin: currentDate });
+      }
+    })
+    .catch(next);
+});
+
+router.post('/signup', async (req, res, next) => {
+  try {
+    let user = await User.create(req.body);
+    user.budget = await user.createBudget();
+    req.login(user, err => (err ? next(err) : res.json(user)));
+  } catch (err) {
+    if (err.name === 'SequelizeUniqueConstraintError') {
+      res.status(401).send('User already exists');
+    } else {
+      next(err);
+    }
+
+  }
+});
+
+router.post('/logout', (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.redirect('/');
+});
+
+router.get('/me', (req, res) => {
+  res.json(req.user);
+});
+
+router.use('/faceID', require('./faceid'));
+// router.use('/google', require('./google'));
