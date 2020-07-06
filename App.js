d7b21ac719ac77f7ec6d525bc28f73c86842f5ed
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2020-07-05 21:54:24 -0400

    Chore: Added Signup component and App name to main component

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Provider } from 'react-redux';
-import { StyleSheet, Text, View } from 'react-native';
-import store from './client/index'
-
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Provider } from "react-redux";
+import { StyleSheet, Text, View } from "react-native";
+import store from "./client/index";
+import { SignUp } from "./client/screens/OnBoard/Signup";
 export default function App() {
   return (
     <Provider store={store}>
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your Hello app!</Text>
+        <Text>WALLET</Text>
+        <SignUp></SignUp>
         <StatusBar style="auto" />
       </View>
     </Provider>
@@ -19,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
