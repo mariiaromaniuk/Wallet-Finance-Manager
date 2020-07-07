b6aa4d8c3ee1f9ee0af365a3321783484cb4eccf
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2020-07-07 01:06:45 -0400

    Chore: Implemented navigation

--- client/screens/OnBoard/Signup.js
+++ client/screens/OnBoard/Signup.js
@@ -1,30 +1,34 @@
 import React, { Component } from "react";
 import { View, Text, TextInput, Button } from "react-native";
 
-export class SignUp extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Username: </Text>
-        <TextInput style={styles.inputStyle} placeholder="Username"></TextInput>
-        <Text>Email: </Text>
-        <TextInput style={styles.inputStyle} placeholder="Email"></TextInput>
-        <Text>Password: </Text>
-        <TextInput style={styles.inputStyle} placeholder="Password"></TextInput>
-        <Text>Confirm Password: </Text>
-        <TextInput
-          style={styles.inputStyle}
-          placeholder="Confirm Password"
-        ></TextInput>
-        <Button title="Sign Up"></Button>
-      </View>
-    );
-  }
+export function SignUpScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>WALLET</Text>
+
+      <Text>Username: </Text>
+      <TextInput style={styles.inputStyle} placeholder="Username"></TextInput>
+      <Text>Email: </Text>
+      <TextInput style={styles.inputStyle} placeholder="Email"></TextInput>
+      <Text>Password: </Text>
+      <TextInput style={styles.inputStyle} placeholder="Password"></TextInput>
+      <Text>Confirm Password: </Text>
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="Confirm Password"
+      ></TextInput>
+      <Button
+        title="Sign Up"
+        onPress={() => navigation.navigate("Details")}
+      ></Button>
+    </View>
+  );
 }
 
 const styles = {
-  viewStyle: {
+  container: {
     flex: 1,
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
