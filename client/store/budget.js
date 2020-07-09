d964fbf9962ff4506ffb4b11519f40c10e22c7a3
Mariia Romaniuk <m.grabelnikova@gmail.com>
2020-07-08 22:36:26 -0400

    Budget store added

---
+++ client/store/budget.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+// Action Types
+const GET_BUDGET = 'GET_BUDGET';
+const UPDATE_BUDGET = 'UPDATE_BUDGET';
+
+// Action Creators
+const getBudget = budget => ({ type: GET_BUDGET, budget });
+const updateBudget = budget => ({ type: UPDATE_BUDGET, budget });
+
+export const fetchBudget = userId => {
+  return async dispatch => {
+    try {
+      const res = await axios.get(`/api/budget/${userId}`);
+      dispatch(getBudget(res.data));
+    } catch (err) {
+      console.log('Error fetching budget: ', err.message);
+    }
+  };
+};
+
+export const setBudget = budget => {
+  return async dispatch => {
+    try {
+      const res = await axios.put(`/api/budget`, budget);
+      dispatch(updateBudget(res.data));
+    } catch (err) {
+      console.log('Error setting budget: ', err.message);
+    }
+  };
+};
+
+const initialState = {};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case GET_BUDGET:
+      return action.budget;
+    case UPDATE_BUDGET:
+      return action.budget;
+    default:
+      return state;
+  }
+};
