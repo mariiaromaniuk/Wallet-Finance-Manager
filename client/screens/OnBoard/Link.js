2f7c5e7cad6000b1d4aecb608eddab95868b5f50
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2020-07-09 14:34:40 -0400

    merged master

--- client/screens/OnBoard/Link.js
+++ client/screens/OnBoard/Link.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import PlaidAuthenticator from 'react-native-plaid-link';
-import { connect } from 'react-redux';
-import { sendToken } from '../../store/token';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import PlaidAuthenticator from "react-native-plaid-link";
+import { connect } from "react-redux";
+import { sendToken } from "../../store/token";
 
 class Link extends React.Component {
   state = {
     data: {},
-    status: 'LOGIN'
+    status: "LOGIN",
   };
 
   render() {
     // Status: CONNECTED|EVENT|ACKOWLEDGE|EXIT|LOGIN
-    switch(this.state.status) {
-      case 'CONNECTED':
-        return this.renderDetails()
-      case 'LOGIN':
-      case 'EXIT':
+    switch (this.state.status) {
+      case "CONNECTED":
+        return this.renderDetails();
+      case "LOGIN":
+      case "EXIT":
         return this.renderButton();
       default:
         return this.renderLogin();
@@ -27,7 +27,7 @@ class Link extends React.Component {
     return (
       <PlaidAuthenticator
         onMessage={this.onMessage}
-        client='Wallet'
+        client="Wallet"
         publicKey="e192da42c496ba424b0a39f9cdb07a"
         env="sandbox"
         product="auth,transactions"
@@ -36,59 +36,66 @@ class Link extends React.Component {
   }
 
   renderButton = () => {
-    return <View style={styles.container}>
-      <TouchableOpacity onPress={() => this.setState({status: ''})}>
-        <Text style={styles.paragraph}>Login with Plaid</Text>
-      </TouchableOpacity>
-    </View>
-  }
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={() => this.setState({ status: "" })}>
+          <Text style={styles.paragraph}>Login with Plaid</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
 
   renderDetails() {
-    console.log('Public token ==============', this.state.data.metadata.public_token)
+    console.log(
+      "Public token ==============",
+      this.state.data.metadata.public_token
+    );
     this.props.sendToken(this.state.data.metadata.public_token);
     return (
       <View style={styles.container}>
-        <Text>This shows after plaid is connected and we get the Public key from user</Text>
+        <Text>
+          This shows after plaid is connected and we get the Public key from
+          user
+        </Text>
       </View>
     );
   }
 
-  onMessage = data => {
+  onMessage = (data) => {
     this.setState({
       data,
-      status: data.action.substr(data.action.lastIndexOf(':') + 1).toUpperCase()
+      status: data.action
+        .substr(data.action.lastIndexOf(":") + 1)
+        .toUpperCase(),
     });
   };
 }
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
-    sendToken: token => dispatch(sendToken(token))
+    sendToken: (token) => dispatch(sendToken(token)),
   };
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     paddingTop: 24,
-    backgroundColor: '#ecf0f1'
+    backgroundColor: "#ecf0f1",
   },
   paragraph: {
     fontSize: 18,
     marginBottom: 5,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#34495e'
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#34495e",
   },
   value: {
     marginBottom: 20,
-    textAlign: 'center'
-  }
+    textAlign: "center",
+  },
 });
 
-export default connect(
-  null,
-  mapDispatch
-)(Link);
+export default connect(null, mapDispatch)(Link);
