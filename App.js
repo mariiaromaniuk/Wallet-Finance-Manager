1faf908ac76847bd25c19fad5e94e1acdad81845
DewinP <dewinspena@gmail.com>
2020-07-10 04:17:33 -0400

    Made stack navigation a little more consistent

--- App.js
+++ App.js
@@ -1,45 +1,68 @@
-import "react-native-gesture-handler";
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, {Component} from "react";
 import { Provider } from "react-redux";
 import { StyleSheet, Text, View } from "react-native";
 import store from "./client/index";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Link from './client/screens/OnBoard/Link'
-import { SignUpScreen } from "./client/screens/OnBoard/Signup";
-import { LogInScreen } from "./client/screens/OnBoard/Login";
+import {
+  Signup,
+  Login,
+  Link,
+  Initial
+} from './client/screens'
 const Stack = createStackNavigator();
 
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
+export default class App extends Component {
+  createHomeStack =() =>{
+    return(
+      <Stack.Navigator>
+        <Stack.Screen
+        name='Initial'
+        component={Initial}
+        options={
+          {
+            title: 'Welcome'
+          }
+        }
+        />
+        <Stack.Screen
+        name='Signup'
+        component={Signup}
+        options={
+          {
+            title: 'Signup'
+          }
+        }
+        />
+        <Stack.Screen
+        name='Login'
+        component={Login}
+        options={
+          {
+            title: 'Login'
+          }
+        }
+        />
+        <Stack.Screen
+        name='Link'
+        component={Link}
+        options={
+          {
+            title: 'Link your bank to Wallet'
+          }
+        }
+        />
+      </Stack.Navigator>
+    )
+  }
 
-export default function App() {
-  return (
-    <Provider store={store}>
+  render(){
+    return(
+      <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Sign Up">
-          <Stack.Screen name="Link Bank" component={Link}/>
-          {/*<Stack.Screen name="Log In" component={LogInScreen}/> 
-          <Stack.Screen name="Sign Up" component={SignUpScreen} />
-          <Stack.Screen name="Details" component={DetailsScreen} /> */}
-        </Stack.Navigator>
-        {/* <StatusBar style="auto" /> */}
+        {this.createHomeStack()}
       </NavigationContainer>
     </Provider>
-  );
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- client/screens/OnBoard/Login.js
+++ client/screens/OnBoard/Login.js
@@ -1,38 +1,41 @@
 import React, { Component } from "react";
-import { View, Text, TextInput, Button } from "react-native";
+import { Container, Text, Item, Form, Input, Button, Label } from "native-base";
 import { connect } from "react-redux";
 
-export function LogInScreen({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <Text>WALLET</Text>
-
-      <Text>Email: </Text>
-      <TextInput style={styles.inputStyle} placeholder="Email"></TextInput>
-      <Text>Password: </Text>
-      <TextInput style={styles.inputStyle} placeholder="Password"></TextInput>
-
+class Login extends Component{
+  constructor(props){
+  super(props);
+  this.state = {email:"", password:""}
+  }
+  render(){
+    return (
+      <Container>
+        <Form>
+        <Item floatingLabel>
+          <Label>Email</Label>
+          <Input
+          onChangeText={text=> this.setState({email:text})}
+          />
+        </Item>
+        <Item floatingLabel secureTextEntry>
+          <Label>Password</Label>
+          <Input
+          onChangeText={text=> this.setState({password:text})}
+          />
+        </Item>
+      </Form>
       <Button
-        title="Log In"
-        onPress={() => navigation.navigate("Details")}
-      ></Button>
-    </View>
-  );
+        block
+        style={{margin:20, marginTop:40}}
+        primary
+        onPress={this.props.handleSubmit(this.state.email, this.state.password)}
+      >
+        <Text>LOGIN</Text>
+      </Button>
+      </Container>
+    );
+  }
 }
-
-const styles = {
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  inputStyle: {
-    height: 40,
-    borderColor: "black",
-    borderWidth: 1,
-  },
-};
 
 const mapDispatch = (dispatch) => {
   return {
@@ -42,4 +45,4 @@ const mapDispatch = (dispatch) => {
   };
 };
 
-export default connect(null, mapDispatch)(LogInScreen);
+export default connect(null, mapDispatch)(Login);
