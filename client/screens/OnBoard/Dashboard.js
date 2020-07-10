08a43f6a7768bae9f9b28de3517788b325cb50ca
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2020-07-10 15:01:23 -0400

    chore: visualizing dummy data

---
+++ client/screens/OnBoard/Dashboard.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import { Container, Text, Item, Form, Input, Button, Label } from "native-base";
+import { connect } from "react-redux";
+import { View, Dimensions } from "react-native";
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart,
+} from "react-native-chart-kit";
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: "", password: "" };
+  }
+  render() {
+    return (
+      <View>
+        <Text>Bezier Line Chart</Text>
+        <LineChart
+          data={{
+            labels: ["January", "February", "March", "April", "May", "June"],
+            datasets: [
+              {
+                data: [
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                ],
+              },
+            ],
+          }}
+          width={Dimensions.get("window").width} // from react-native
+          height={220}
+          yAxisLabel="$"
+          yAxisSuffix="k"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            backgroundColor: "#e26a00",
+            backgroundGradientFrom: "#fb8c00",
+            backgroundGradientTo: "#ffa726",
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+            propsForDots: {
+              r: "6",
+              strokeWidth: "2",
+              stroke: "#ffa726",
+            },
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    handleSubmit(email, password) {
+      // dispatch(LogInScreen(email, password));
+    },
+  };
+};
+
+export default connect(null, mapDispatch)(Dashboard);
