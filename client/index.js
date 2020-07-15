abeea9b1fe4e28a352787a76c6f037b8ab80e131
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2020-07-15 15:51:24 -0400

    included dashboard store to global store

--- client/index.js
+++ client/index.js
@@ -3,10 +3,12 @@ import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import user from "./store/user";
-import token from './store/token'
+import token from "./store/token";
+import dashboard from "./store/Dashboard";
 const reducer = combineReducers({
   user,
-  token
+  token,
+  dashboard,
 });
 
 const middleware = composeWithDevTools(
